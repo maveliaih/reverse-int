219cb5148715269d6439b38d6212440ad8ce7ead
Daniil <slimbo.you@gmail.com>
2022-06-27 20:01:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString()
+
+    let result = ''
+
+    for (let i = str.length; i >= 0; i--)
+    {
+        result += str.charAt(i)
+    }
+    return parseInt(result)
 }
